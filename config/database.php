2fa58ed5a32dca8a0fab2d102f6be3922fe49c31
Saadu <saadum603@gmail.com>
2024-04-09 11:11:10 +0300

    comm

--- config/database.php
+++ config/database.php
@@ -36,7 +36,7 @@ return [
     'connections' => [
 
         'pgsql' => [
-            'driver' => 'pgsql',
+            'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
